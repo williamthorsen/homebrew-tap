d460c5bfe5d40315a2c34f42b319a3a5a567315b
William Thorsen <william@thorsen.dev>
2024-08-16 04:07:41 -0700

    git-recon | Release v0.7.3

--- Formula/git-recon.rb
+++ Formula/git-recon.rb
@@ -1,8 +1,8 @@
 class GitRecon < Formula
   desc "Git-Recon custom Git configuration"
   homepage "https://github.com/williamthorsen/git-recon"
-  url "https://github.com/williamthorsen/git-recon/archive/v0.7.1.tar.gz"
-  sha256 "2a0121f47439973b4f7bb72b70036fa43d01530c7ec2717939abcc950b2834c8"
+  url "https://api.github.com/repos/williamthorsen/git-recon/tarball/v0.7.3"
+  sha256 "a262de5376699bb99c6bc3cfc488e203d5e7ef01ef3aa6134b4b7937bd017498"
 
   def install
     (etc/"gitconfig.d").install "git/git-recon.gitconfig"
