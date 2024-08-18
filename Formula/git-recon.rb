a4818a5e724e25ec7eefcfda9ed54ca5ec5435d4
William Thorsen <william@thorsen.dev>
2024-08-18 06:01:13 -0700

    git-recon: Release v0.8.3

--- Formula/git-recon.rb
+++ Formula/git-recon.rb
@@ -1,24 +1,28 @@
 class GitRecon < Formula
   desc "Git-Recon custom Git configuration"
   homepage "https://github.com/williamthorsen/git-recon"
-  url "https://github.com/williamthorsen/git-recon/archive/refs/tags/v0.8.2.tar.gz"
-  sha256 "f4b39c81b7b66b2c5efab55bf70cb7b2402a781554c76a5250bc0dbfd3ffcb98"
-  version "0.8.2"
+  url "https://github.com/williamthorsen/git-recon/archive/refs/tags/v0.8.3.tar.gz"
+  sha256 "a78d63417b9b14586750d0389520d812f32c2753f85b45a3d10816fea1a0db27"
+  version "0.8.3"
 
   def install
-    (etc/"gitconfig.d").install "git/git-recon.gitconfig"
+    # Install the configuration file in a shared directory
+    (share/"gitconfig.d").install "git/git-recon.gitconfig"
+
+    # Install the executable script
     bin.install "bin/git-recon.sh" => "git-recon"
   end
 
-  def caveats
-    <<~EOS
-      ==> Important Caveats
+  def post_install
+      # Prompt the user to run the installer
+      opoo <<~EOS
+        Git-Recon has been installed successfully.
 
-      Git-Recon uses custom aliases, which are defined in a single file, "git-recon.gitconfig".
+        To enable the Git-Recon aliases, please run the following command:
 
-      To include this file in your ~/.gitconfig, run the following command:
+          git-recon --install
 
-        git-recon --install
-    EOS
-  end
+        This will automatically add the necessary `include` directive to your ~/.gitconfig file.
+      EOS
+    end
 end
